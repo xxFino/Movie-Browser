2f03f1f3ddbca5c0c6237262459bdecf66a9d3f4
Marek Wal <walmarek1987@gmail.com>
2023-04-01 15:53:32 +0200

    index.js moved to src

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from '../reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 import { Normalize } from 'styled-normalize';
 import { Provider } from 'react-redux';
-import { GlobalStyle } from './GlobalStyle';
+import { GlobalStyle } from './core/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
