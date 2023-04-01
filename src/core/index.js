d2b5d44471f0c2a9706dbeaf031cddb590242b9e
Marek Wal <walmarek1987@gmail.com>
2023-04-01 15:51:07 +0200

    ThemeProvider added

--- src/core/index.js
+++ src/core/index.js
@@ -5,16 +5,18 @@ import reportWebVitals from '../reportWebVitals';
 import { Normalize } from 'styled-normalize';
 import { Provider } from 'react-redux';
 import { GlobalStyle } from './GlobalStyle';
+import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
 root.render(
   <React.StrictMode>
-     <Provider store={store}>
+    <Provider store={store}>
+    <ThemeProvider theme={theme}>
       <App />
       <Normalize/>
       <GlobalStyle />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
