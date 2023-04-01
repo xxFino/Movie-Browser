972374026e787c6f928d5975adc1fcc795950def
Marek Wal <walmarek1987@gmail.com>
2023-04-01 15:40:12 +0200

    GlobalStyle.js created

---
+++ src/core/GlobalStyle.js
@@ -0,0 +1,17 @@
+export const GlobalStyle = createGlobalStyle`
+
+html {
+    box-sizing: border-box;
+}
+
+*,
+::after,
+::before {
+    box-sizing: inherit;
+}
+
+body {
+   font-family: 'Lato', sans-serif;
+   font-style: normal;
+}
+`
