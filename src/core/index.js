1140b21f96ad33efa0d8a4e89283cbf4549e50df
Marek Wal <walmarek1987@gmail.com>
2023-04-01 15:46:46 +0200

    Provider, GlobalStyle added

--- src/core/index.js
+++ src/core/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from '../reportWebVitals';
 import { Normalize } from 'styled-normalize';
+import { Provider } from 'react-redux';
+import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-    <Normalize/>
-    <App />
+     <Provider store={store}>
+      <App />
+      <Normalize/>
+      <GlobalStyle />
+    </Provider>
   </React.StrictMode>
 );
 
