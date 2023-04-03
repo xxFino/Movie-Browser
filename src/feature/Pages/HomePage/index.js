0a1874835f7b37f11c051c5b982cfa7cb29b2e0e
Your Name <you@example.com>
2023-04-03 13:38:40 +0200

    ganres map

--- src/feature/Pages/HomePage/index.js
+++ src/feature/Pages/HomePage/index.js
@@ -2,6 +2,7 @@ import {
   Box,
   Content,
   ContentBox,
+  ContentTags,
   Date,
   Description,
   Images,
@@ -9,42 +10,62 @@ import {
   Paragraph,
   Rating,
   Tag,
+  TagBox,
+  TagTile,
   Text,
   TileMovie,
   Title,
   Wrapper,
 } from "./styled";
 import { ReactComponent as Icon } from "../../../core/icon/Vector.svg";
+import { genres } from "../ganres";
+
 
 export const HomePage = ({ movies }) => {
   return (
     <Content>
       <Title>Popular movies</Title>
       <Wrapper>
-        {movies.map(({ id, title, vote_average, imageUrl, vote_count,release_date}) => {
-          return (
-            <TileMovie key={id}>
-              <Box>
-              <Images src={imageUrl} alt="Movie Poster" />
-              </Box>
-              <ContentBox>
-              <Description>
-                <Name>{title}</Name>
-                <Date>{release_date}</Date>
-              </Description>
-              <Description>
-              <Tag>tagi</Tag>
-              </Description>
-              <Rating>
-                <Icon/>
-                <Text>{vote_average}</Text>
-                <Paragraph>{vote_count} votes </Paragraph>
-                <p>{}</p>
-              </Rating>
-              </ContentBox>
-            </TileMovie>
-          );
-        })}
+        {movies.map(
+          ({
+            id,
+            title,
+            vote_average,
+            imageUrl,
+            vote_count,
+            release_date,
+            genre_ids,
+          }) => {
+            return (
+              <TileMovie key={id}>
+                <Box>
+                  <Images src={imageUrl} alt="Movie Poster" />
+                </Box>
+                <ContentBox>
+                  <Description>
+                    <Name>{title}</Name>
+                    <Date>{release_date}</Date>
+                  </Description>
+                </ContentBox>
+                <ContentTags>
+                  {genre_ids.map((genreId) => (
+                    <TagBox key={genreId}>
+                      <TagTile>
+                        {genres.find((genre) => genre.id === genreId)?.name}
+                      </TagTile>
+                    </TagBox>
+                  ))}
+                </ContentTags>
+
+                <Rating>
+                  <Icon />
+                  <Text>{vote_average}</Text>
+                  <Paragraph>{vote_count} votes </Paragraph>
+                </Rating>
+              </TileMovie>
+            );
+          }
+        )}
       </Wrapper>
     </Content>
   );
