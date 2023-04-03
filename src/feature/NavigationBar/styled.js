f214a25cf8755cd7227ff4bc0f6568240e9a6124
Marek Wal <walmarek1987@gmail.com>
2023-04-03 20:36:52 +0200

    NavigationBar component fixed

--- src/feature/NavigationBar/styled.js
+++ src/feature/NavigationBar/styled.js
@@ -48,34 +48,10 @@ export const NavList = styled.ul`
 export const NavItem = styled.li`
   list-style: none;
 `;
+
 export const StyledNavLink = styled(NavLink).attrs(() => ({
   activeClassName,
 }))`
   color: #ffffff;
   list-style: none;
 `;
-
-import { NavLink } from "react-router-dom"
-import styled from "styled-components"
-
-const activeClassName = "active";
-
-export const NavBar = styled.nav`
-width: 100%;
-height: 94px;
-background: #18181B;
-
-`
-export const NavList = styled.ul`
-list-style: none;
-display: flex;
-`
-
-export const NavItem = styled.li`
-
-`
-export const StyledNavLink = styled(NavLink).attrs(() => ({
-    activeClassName,
-  }))`
-  color: #ffffff;
-  `
