95adf2553420dcbcfd20cd55369c5669018f1df4
Marek Wal <walmarek1987@gmail.com>
2023-04-03 20:12:35 +0200

    NavigationBar styling added

---
+++ src/feature/NavigationBar/index.js
@@ -0,0 +1,58 @@
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { MovieList } from "../MovieList";
+import { MoviePage } from "../MoviePage";
+import { People } from "../People";
+import { Profile } from "../Profile";
+import { SearchBar } from "../SearchBar";
+import {
+  Header,
+  NavBar,
+  NavItem,
+  NavList,
+  StyledNavLink,
+  StyledVideoIcon,
+  Title,
+} from "./styled";
+
+export const NavigationBar = () => {
+  return (
+    <HashRouter>
+      <Header>
+        <NavBar>
+          <NavList>
+            <Title>
+              <StyledNavLink to="/movie-list">
+                <StyledVideoIcon />
+                Movies Browser
+              </StyledNavLink>
+            </Title>
+            <NavItem>
+              <StyledNavLink to="/movie-list">MOVIES</StyledNavLink>
+            </NavItem>
+            <NavItem>
+              <StyledNavLink to="/people">PEOPLE</StyledNavLink>
+            </NavItem>
+          </NavList>
+          <SearchBar />
+        </NavBar>
+      </Header>
+      <Switch>
+        <Route path="/movie-list/">
+          <MovieList />
+        </Route>
+        <Route path="/movie-list/:id">
+          <MoviePage />
+        </Route>
+        <Route path="/people">
+          <People />
+        </Route>
+        <Route path="/people/:id">
+          <Profile />
+        </Route>
+        <Route path="/">
+          <Redirect to="movie-list" />
+        </Route>
+      </Switch>
+    </HashRouter>
+  );
+};
