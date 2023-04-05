d182272ae0e3f8ef9b92cf41522eed138d07789d
Your Name <you@example.com>
2023-04-05 10:35:39 +0200

    Header component created

---
+++ src/core/components/Header/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default styled.h1`
+  margin: 0px 0 24px 0;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 1.2;
+  @media (max-width: 480px) {
+    margin: 24px 0 12px 0;
+    font-size: 18px;
+    margin: 0px 0 12px 0;
+
+  } ;
+`;
