3a4f4f19a9a6411bda7cebafc4314459fad9e5f0
Your Name <you@example.com>
2023-04-05 11:57:19 +0200

    h1 to h2

--- src/core/components/Header/index.js
+++ src/core/components/Header/index.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
-export default styled.h1`
+export default styled.h2`
   margin: 0px 0 24px 0;
   font-weight: 600;
   font-size: 36px;
-  line-height: 1.2;
   @media (max-width: 480px) {
     margin: 24px 0 12px 0;
     font-size: 18px;
