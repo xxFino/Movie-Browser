924d2672914f13c8a9759e54a62402cf5fe3866a
Your Name <you@example.com>
2023-04-06 09:08:11 +0200

    Loading component created

--- src/feature/Content/Loading/index.js
+++ src/feature/Content/Loading/index.js
@@ -1,3 +1,14 @@
-export const Loading=() => {
-    <div>ładowanie</div>
-}
+import { Container } from "../../../core/components/Container";
+import Header from "../../../core/components/Header";
+import { StyledSpinner, Wrapper } from "./styled";
+
+export const Loading = () => {
+  return (
+    <Container>
+      <Header>Please wait </Header>
+      <Wrapper>
+        <StyledSpinner />
+      </Wrapper>
+    </Container>
+  );
+};
