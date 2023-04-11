77866b7f3948b02e0477c0ca64d3bbd6d00ea62c
Your Name <you@example.com>
2023-04-11 13:26:08 +0200

    margin change

--- src/core/components/Header/index.js
+++ src/core/components/Header/index.js
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
 export default styled.h2`
-  margin: 150px 0 24px 0;
+  margin: 56px 0 24px 0;
   font-weight: 600;
   font-size: 36px;
   word-break: break-word;
   @media (max-width: 480px) {
-    margin: 166px 0 12px 0;
+    margin: 24px 0px 12px;
     font-size: 18px;
   }
-  @media (max-width: 1380px) {
+  @media (max-width: 1396px) {
     font-size: 28px;
   }
   @media (max-width: 776px) {
