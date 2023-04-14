921f3fe6434dc49e2727723579edacf628252f3d
Your Name <you@example.com>
2023-04-14 11:22:20 +0200

    peopleSaga and peopleReducer added

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
 import { moviesSaga } from "../feature/Movie/moviesSaga";
+import { peopleSaga } from "../feature/People/peopleSaga";
 
 export default function* rootSaga() {
-  yield all([moviesSaga()]);
+  yield all([moviesSaga(),peopleSaga()]);
 }
