f284408a8c442f3fa058717d0edd5672eee4e388
Your Name <you@example.com>
2023-04-14 14:58:36 +0200

    Video svg added and fotmated code

---
+++ src/feature/Movie/MoviesList/index.js
@@ -0,0 +1,58 @@
+import {
+  MovieInfoBox,
+  MovieYear,
+  MovieImage,
+  MoviesTitle,
+  MovieRating,
+  MovieTile,
+  Wrapper,
+  MovieRatingBox,
+} from "./styled";
+import { ReactComponent as Icon } from "../../../core/icon/Vector.svg";
+import { Genres } from "./Genres";
+import Header from "../../../core/components/Header";
+import { TextBold, TextNormal } from "../../../core/components/Text";
+
+export const MoviesList = ({ movies}) => {
+return(
+  <>
+<Header>Popular movies</Header>
+        <Wrapper>
+          {movies &&
+            movies.map(
+              ({
+                id,
+                title,
+                vote_average,
+                imageUrl,
+                vote_count,
+                release_date,
+                genre_ids,
+              }) => {
+                return (
+                  <MovieTile key={id}>
+                    <MovieImage src={imageUrl} alt="Movie Poster" />
+                    <MovieInfoBox>
+                      <MoviesTitle>{title}</MoviesTitle>
+                      <MovieYear>
+                        {new Date(release_date).getFullYear()}
+                      </MovieYear>
+                      <Genres genre_ids={genre_ids} />
+                      <MovieRating>
+                        <MovieRatingBox>
+                        <Icon />
+                        <TextBold>{vote_average}</TextBold>
+                        <TextNormal>
+                          {vote_count} votes
+                        </TextNormal>
+                        </MovieRatingBox>
+                      </MovieRating>
+                    </MovieInfoBox>
+                  </MovieTile>
+                );
+              }
+            )}
+        </Wrapper>
+        </>
+)
+};
