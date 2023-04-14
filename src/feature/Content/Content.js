e16ae2e5bf13f06118811b529604f8e8797f0bb6
Your Name <you@example.com>
2023-04-14 10:07:11 +0200

    removed Content.js and created status in Movies.js

--- src/feature/Content/Content.js
+++ src/feature/Content/Content.js
@@ -1,20 +1,5 @@
 import { Error } from "./Error";
 import { MoviesList } from "../Movie/MovieList";
 import { Loading } from "./Loading";
+import { People } from "../People";
 
-export const Content = ({ status, movies }) => {
-    switch (status) {
-      case "initial":
-        return null;
-      case "loading":
-        return <p><Loading/></p>;
-      case "error":
-        return <Error />;
-      case "success":
-        return <MoviesList movies={movies} />;
-  
-      default:
-        throw new Error(`incorrect status: ${status}`);
-    }
-  };
-  
--- src/feature/Movie/Movies.js
+++ src/feature/Movie/Movies.js
@@ -1,16 +1,31 @@
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMovies, selectMovies, selectMoviesStatus } from "./moviesSlice";
-import { Content } from "../Content/Content";
+import { NoResult } from "../Content/NoResult";
+import { Loading } from "../Content/Loading";
+import { MoviesList } from "./MoviesList";
+import { Pagination } from "../../core/components/Pagination";
+import { Container } from "../../core/components/Container";
+import { Error } from "../Content/Error";
 import { useEffect } from "react";
 
 export const Movies = () => {
-    const dispatch = useDispatch();
-    const moviesStatus = useSelector(selectMoviesStatus);
-    const movies = useSelector(selectMovies);
-  
-    useEffect(() => {
-      dispatch(fetchMovies());
-    }, [dispatch]);
-  
-    return <Content status={moviesStatus} movies={movies} />;
-  };
+  const dispatch = useDispatch();
+  const moviesStatus = useSelector(selectMoviesStatus);
+  const movies = useSelector(selectMovies);
+
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
+
+  return {
+    noResult: <NoResult />,
+    loading: <Loading />,
+    success: (
+      <Container>
+        <MoviesList status={moviesStatus} movies={movies} />
+
+      </Container>
+    ),
+    error: <Error />,
+  }[moviesStatus];
+};
