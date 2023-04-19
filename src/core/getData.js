094ea98a35ed8e92f5d376cbbdbc0c4ab03a6e9b
Your Name <you@example.com>
2023-04-19 08:41:51 +0200

    getData changed

--- src/core/getData.js
+++ src/core/getData.js
@@ -3,33 +3,18 @@ import axios from "axios";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = "64dcf25583202648eae7e90e51a0966d";
 
-export const getMoviesData = async (currentPage) => {
-  const response = await axios.get(
-    `${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${currentPage}`
-  );
-  const movies = response.data.results.map((movie) => {
-    const imageUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
-    return { ...movie, imageUrl };
-  });
-  const totalPages = response.data.total_pages;
-  const totalResults = response.data.total_results;
-  return { movies, totalPages, totalResults };
+export const getMoviesData = async (page) => {
+  const { data } = await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${page}`);
+  const movies = data.results.map(movie => ({ ...movie, imageUrl: `https://image.tmdb.org/t/p/original${movie.poster_path}` }));
+  return { movies, totalPages: data.total_pages, totalResults: data.total_results };
 };
 export const getGenres = () =>
   axios
     .get(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}`)
     .then((response) => response.data.genres);
 
-export const getPersonData = async (currentPage) => {
-  const response = await axios.get(
-    `${BASE_URL}/person/popular?api_key=${API_KEY}&page=${currentPage}`
-  );
-  const people = response.data.results.map((people) => {
-    const imageUrl = `https://image.tmdb.org/t/p/original${people.profile_path}`;
-
-    return { ...people, imageUrl };
-  });
-  const totalPages = response.data.total_pages;
-  const totalResults = response.data.total_results;
-  return { people, totalPages, totalResults };
+export const getPersonData = async (page) => {
+  const {data} = await axios.get(`${BASE_URL}/person/popular?api_key=${API_KEY}&page=${page}`);
+ const people =  data.results.map(people => ({ ...people, imageUrl: `https://image.tmdb.org/t/p/original${people.profile_path}` }));
+  return { people, totalPages: data.total_pages, totalResults: data.total_results };
 };
