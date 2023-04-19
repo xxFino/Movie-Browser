4e9bc2b9c005babc17c98aa09c7a1c99e9b77960
Your Name <you@example.com>
2023-04-19 08:46:55 +0200

    pages added

--- src/feature/Movie/Movies.js
+++ src/feature/Movie/Movies.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMovies, selectMovies, selectMoviesStatus } from "./moviesSlice";
+import { fetchMovies, selectMovies, selectMoviesStatus, selectTotalPages } from "./moviesSlice";
 import { NoResult } from "../Content/NoResult";
 import { Loading } from "../Content/Loading";
 import { MoviesList } from "./MoviesList";
@@ -8,14 +8,15 @@ import { Error } from "../Content/Error";
 import { useEffect } from "react";
 import { Pagination } from "../../core/components/Pagination";
 
-export const Movies = () => {
+export const Movies = ({page}) => {
   const dispatch = useDispatch();
   const moviesStatus = useSelector(selectMoviesStatus);
   const movies = useSelector(selectMovies);
+  const pages= useSelector(selectTotalPages);
 
   useEffect(() => {
-    dispatch(fetchMovies());
-  }, [dispatch]);
+    dispatch(fetchMovies(page));
+  }, [dispatch,page]);
 
   return {
     noResult: <NoResult />,
@@ -23,7 +24,7 @@ export const Movies = () => {
     success: (
       <Container>
         <MoviesList status={moviesStatus} movies={movies} />
-        <Pagination />
+        <Pagination totalPages={pages} />
       </Container>
     ),
     error: <Error />,
