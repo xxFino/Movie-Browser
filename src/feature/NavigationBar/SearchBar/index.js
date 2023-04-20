dbb9185ab1c818d4f81e367a1dd2f9158c907988
Your Name <you@example.com>
2023-04-20 11:55:57 +0200

    location,history,querry added and created onInputChange

--- src/feature/NavigationBar/SearchBar/index.js
+++ src/feature/NavigationBar/SearchBar/index.js
@@ -1,10 +1,35 @@
-import { Form, SearchInput, StyledSearchIcon} from "./styled"
+import { useHistory, useLocation } from "react-router-dom";
+import { Form, SearchInput, StyledSearchIcon } from "./styled";
+import searchQueryParamName from "../../Movie/searchQueryParamName";
 
 export const SearchBar = () => {
-    return (
-        <Form>
-            <StyledSearchIcon />
-        <SearchInput type="text" placeholder="Search for movies..." />
-        </Form>
-    );
+  const location = useLocation();
+  const history = useHistory();
+  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+
+  const onInputChange = ({ target }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    if (target.value.trim() === "") {
+      searchParams.delete(searchQueryParamName);
+    } else {
+      searchParams.set(searchQueryParamName, target.value);
+    }
+    history.push(`${location.pathname}?${searchParams.toString()}`);
+  };
+
+  return (
+    <Form>
+      <StyledSearchIcon />
+      <SearchInput
+        placeholder={
+          location.pathname.includes("people")
+            ? "Search for people..."
+            : "Search for movies..."
+        }
+        value={query || ""}
+        onChange={onInputChange}
+      />
+    </Form>
+  );
 };
