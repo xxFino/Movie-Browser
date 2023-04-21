64c2f1e7c67d5553d6027c5480ac54e460ee9b36
Your Name <you@example.com>
2023-04-21 12:54:30 +0200

    profilePath added in PeopleList and removed it from getData and changed getMoviesData and getPersonData on getPopulatData

--- src/feature/People/PeopleList/index.js
+++ src/feature/People/PeopleList/index.js
@@ -1,18 +1,28 @@
 import Header from "../../../core/components/Header";
 import { Photo, Tile, Wrapper } from "./styled";
+import Poster from "../../Movie/MoviesList/Poster.svg";
 
 export const PeopleList = ({ people }) => {
+  const profilePath = `https://image.tmdb.org/t/p/original`;
   return (
     <>
       <Header>People</Header>
       <Wrapper>
         {people &&
-          people.map(({ id, name, imageUrl }) => (
-            <Tile key={id}>
-              <div>
-                <Photo src={imageUrl} alt="photo" />
-                <p>{name}</p>
-              </div>
+          people.map((person) => (
+            <Tile key={person.id}>
+              {person.profile_path ? (
+                <Photo
+                  src={
+                    person.profile_path &&
+                    `${profilePath}${person.profile_path}`
+                  }
+                  alt=""
+                />
+              ) : (
+                <Photo src={Poster} alt="poster" />
+              )}
+              <p>{person.name}</p>
             </Tile>
           ))}
       </Wrapper>
