0eac3c2fef37f82a8e894148deb56d013cdc2ce8
Your Name <you@example.com>
2023-04-23 15:02:08 +0200

    removed query

--- src/core/getData.js
+++ src/core/getData.js
@@ -3,11 +3,10 @@ import axios from "axios";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = "64dcf25583202648eae7e90e51a0966d";
 
-export const getPopularData = async (dataName, page,query) => {
+export const getPopularData = async (dataName, page) => {
   const { data } = await axios.get(
-    `${BASE_URL}/${dataName}/popular?api_key=${API_KEY}&page=${page}${query}`
+    `${BASE_URL}/${dataName}/popular?api_key=${API_KEY}&page=${page}`
   );
-
   return {
     people: data.results,
     movies: data.results,
