53a288f53986b6a9ac1f35d0c4f31153081ae4fc
Your Name <you@example.com>
2023-04-25 13:21:37 +0200

    Headre changed and title added

--- src/feature/People/PeopleList/index.js
+++ src/feature/People/PeopleList/index.js
@@ -3,18 +3,26 @@ import { Photo, Wrapper } from "./styled";
 import Poster from "../../Movie/MoviesList/Poster.svg";
 import { toActor } from "../../NavigationBar/route";
 import { Tile } from "../../Movie/MovieTile/styled";
+import { useSelector } from "react-redux";
+import { selectTotalResult } from "../peopleSlice";
+import { useLocation } from "react-router-dom";
+import searchQueryParamName from "../../NavigationBar/SearchBar/searchQueryParamName";
 
 export const PeopleList = ({ people }) => {
+  const totalResult = useSelector(selectTotalResult);
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+  const title = query
+    ? `Search results for "${query}" (${totalResult})`
+    : "Popular People";
   const profilePath = `https://image.tmdb.org/t/p/original`;
   return (
     <>
-      <Header>People</Header>
+      <Header>{title}</Header>
       <Wrapper>
         {people &&
           people.map((person) => (
-           
             <Tile to={toActor(person.id)} key={person.id}>
-    
               {person.profile_path ? (
                 <Photo
                   src={
