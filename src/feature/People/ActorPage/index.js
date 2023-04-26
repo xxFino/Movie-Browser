8a5d6408e659508c4bb339383d3522fb5798b0a4
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-04-26 15:27:30 +0200

    created movie cast and movie crew in actor page

--- src/feature/People/ActorPage/index.js
+++ src/feature/People/ActorPage/index.js
@@ -1,14 +1,31 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import {
+  selectMoviesCast,
+  selectMoviesCrew,
+} from "./actorSlice";
+import { Title } from "../../../core/components/Title";
 import { Container } from "../../../core/components/Container";
+import { MoviesList } from "../../Movie/MoviesList";
 
-export const ActorInfo = ({ actorInfo }) => {
-    return (
-      <Container>
-        <p>aktor </p>
-        {actorInfo && (
-          <div key={actorInfo.id}>
-            <p>{actorInfo.name}</p>
-          </div>
-        )}
-      </Container>
-    );
-  };
+export const ActorInfo = () => {
+  const cast = useSelector(selectMoviesCast);
+  const crew = useSelector(selectMoviesCrew);
+
+  return (
+    <>
+      {cast.length > 0 && (
+        <Container>
+          <Title title={`Movies - cast (${cast.length})`} />
+          <MoviesList movies={cast} />
+        </Container>
+      )}
+      {crew.length > 0 && (
+        <Container>
+          <Title title={`Movies - crew (${crew.length})`} />
+          <MoviesList movies={crew} />
+        </Container>
+      )}
+    </>
+  );
+};
