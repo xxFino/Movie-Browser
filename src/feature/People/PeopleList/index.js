226d5d74b26ed04e9e5e6c8f321e1770279f8b71
Your Name <you@example.com>
2023-04-27 16:25:28 +0200

    I moved to PeopleTile

--- src/feature/People/PeopleList/index.js
+++ src/feature/People/PeopleList/index.js
@@ -1,12 +1,9 @@
-import Header from "../../../core/components/Header";
-import { Photo, Wrapper } from "./styled";
-import Poster from "../../Movie/MoviesList/Poster.svg";
-import { toActor } from "../../NavigationBar/route";
-import { Tile } from "../../Movie/MovieTile/styled";
 import { useSelector } from "react-redux";
-import { selectTotalResults } from "../peopleSlice";
 import { useLocation } from "react-router-dom";
 import searchQueryParamName from "../../NavigationBar/SearchBar/searchQueryParamName";
+import { PeopleTile } from "../PeopleTile";
+import Header from "../../../core/components/Header";
+import { selectTotalResults } from "../peopleSlice";
 
 export const PeopleList = ({ people }) => {
   const totalResult = useSelector(selectTotalResults);
@@ -15,28 +12,12 @@ export const PeopleList = ({ people }) => {
   const title = query
     ? `Search results for "${query}" (${totalResult})`
     : "Popular People";
-  const profilePath = `https://image.tmdb.org/t/p/original`;
+
   return (
     <>
       <Header>{title}</Header>
       <Wrapper>
-        {people &&
-          people.map((person) => (
-            <Tile to={toActor(person.id)} key={person.id}>
-              {person.profile_path ? (
-                <Photo
-                  src={
-                    person.profile_path &&
-                    `${profilePath}${person.profile_path}`
-                  }
-                  alt=""
-                />
-              ) : (
-                <Photo src={Poster} alt="poster" />
-              )}
-              <p>{person.name}</p>
-            </Tile>
-          ))}
+        <PeopleTile people={people} />
       </Wrapper>
     </>
   );
