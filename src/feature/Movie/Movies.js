5507620a8b3f500e6c31de3383ed3f9dd746c33a
Your Name <you@example.com>
2023-04-27 11:41:41 +0200

    selecMoviesByQuery removed , created searchResults and I added to in the code to display search lists

--- src/feature/Movie/Movies.js
+++ src/feature/Movie/Movies.js
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchMovies,
-  selectMoviesByQuery,
+  selectMovies,
   selectMoviesStatus,
+  selectTotalPages,
 } from "./moviesSlice";
 import { NoResult } from "../Content/NoResult";
 import { Loading } from "../Content/Loading";
@@ -14,39 +15,48 @@ import { Pagination } from "../../core/components/Pagination";
 import { useLocation } from "react-router-dom";
 import searchQueryParamName from "../NavigationBar/SearchBar/searchQueryParamName";
 import { useState } from "react";
+import { getQueryData } from "../../core/getData";
 
 export const Movies = () => {
   const location = useLocation();
-  const query = new URLSearchParams(location.search).get(searchQueryParamName);
-
+  const query = new URLSearchParams(location.search || "").get(
+    searchQueryParamName
+  );
   const dispatch = useDispatch();
   const moviesStatus = useSelector(selectMoviesStatus);
-  const movies = useSelector((state) => selectMoviesByQuery(state, query));
+  const movies = useSelector(selectMovies);
+  const [searchResults, setSearchResults] = useState([]);
   const [page, setPage] = useState(1);
-  const totalPages = 500;
-
-  const onPageChange = (page) => {
-    setPage(page);
-    dispatch(fetchMovies({ page }));
-  };
+  const totalPages = useSelector(selectTotalPages);
 
   useEffect(() => {
-    dispatch(fetchMovies({ page: page }));
-  }, [dispatch, page]);
+    query
+      ? getQueryData("movie", query).then(setSearchResults)
+      : dispatch(fetchMovies({ page: 1 }));
+  }, [dispatch, query]);
+
+  const onPageChange = (page) => (
+    setPage(page), dispatch(fetchMovies({ page }))
+  );
 
   return {
-    noResult: <NoResult />,
     loading: <Loading />,
+    error: <Error />,
+    noResult: <NoResult />,
     success: (
       <Container>
-        <MoviesList status={moviesStatus} movies={movies} />
-        <Pagination
-          page={page}
-          totalPages={totalPages}
-          onPageChange={onPageChange}
+        <MoviesList
+          status={moviesStatus}
+          movies={query ? searchResults : movies}
         />
+        {movies > 20 && (
+          <Pagination
+            page={page}
+            totalPages={totalPages}
+            onPageChange={onPageChange}
+          />
+        )}
       </Container>
     ),
-    error: <Error />,
   }[moviesStatus];
 };
