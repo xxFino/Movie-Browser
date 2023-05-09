f687e9cffefadcf2785ea14bf38d6bf7f165743f
Marek Wal <walmarek1987@gmail.com>
2023-05-09 17:30:03 +0200

    movieDetails & movieCredits added

--- src/feature/Movie/MoviePage/MoviePageDetails/index.js
+++ src/feature/Movie/MoviePage/MoviePageDetails/index.js
@@ -4,14 +4,14 @@ import {
   selectMoviePageDetails,
 } from "../moviePageSlice";
 import { Backdrop } from "./Backdrop";
+import { MovieDetails } from "./MovieDetails";
+import { MovieCredits } from "./MovieCredits";
 
 export const MoviePageDetails = () => {
   const movieDetails = useSelector(selectMoviePageDetails);
   const movieCredits = useSelector(selectMoviePageCredits);
   const movieCast = movieCredits.cast;
   const movieCrew = movieCredits.crew;
-  console.log(movieCast);
-  console.log(movieCrew);
 
   return (
     <>
@@ -21,6 +21,24 @@ export const MoviePageDetails = () => {
           title={movieDetails.title}
           rate={movieDetails.vote_average}
           votes={movieDetails.vote_count}
+        />
+      )}
+      {movieDetails.title && (
+        <MovieDetails
+        poster_path={movieDetails.poster_path}
+        title={movieDetails.title}
+        release={movieDetails.release_date}
+        production={movieDetails.production_countries}
+        genres={movieDetails.genres}
+        rate={movieDetails.vote_average}
+        votes={movieDetails.vote_count}
+        details={movieDetails.overview}
+    />
+      )}
+      {movieCredits && (
+        <MovieCredits
+        cast={movieCast}
+        crew={movieCrew}
         />
       )}
     </>
