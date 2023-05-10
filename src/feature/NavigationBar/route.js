ef0e7b58b8fb63b8e97e7b0cfd707cf41489e633
Marek Wal <walmarek1987@gmail.com>
2023-05-10 10:09:47 +0200

    ActorSaga.js renemed to actorSaga.js

--- src/feature/NavigationBar/route.js
+++ src/feature/NavigationBar/route.js
@@ -1,4 +1,4 @@
-export const toMovie = (id) => `/movie/${id}`;
+export const toMovie = (id) => `/movies/movie/${id}`;
 export const toMovies = () => "/movies";
-export const toActor = (id) => `/person/${id}`;
+export const toActor = (id) => `/people/person/${id}`;
 export const toPeople = () => "/people";
