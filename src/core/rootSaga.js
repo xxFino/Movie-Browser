c3c0a04d432400c1f6eb815e2f047e3e55a1b4fa
Marek Wal <walmarek1987@gmail.com>
2023-05-12 12:21:42 +0200

    ActorSaga.js renamed to actorSaga.js

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -2,7 +2,7 @@ import { all } from "redux-saga/effects";
 import { moviesSaga } from "../feature/Movie/moviesSaga";
 import { peopleSaga } from "../feature/People/peopleSaga";
 import { moviePageSaga } from "../feature/Movie/MoviePage/moviePageSaga";
-import { actorSaga } from "../feature/People/ActorPage/ActorSaga";
+import { actorSaga } from "../feature/People/ActorPage/actorSaga";
 
 export default function* rootSaga() {
   yield all([moviesSaga(), peopleSaga(), actorSaga(), moviePageSaga()]);
